d9724358d68426a751e96894c18800cff0f5a2ab
ilxmaniiloxx <109387375+ilxmaniiloxx@users.noreply.github.com>
2022-07-15 16:55:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+function setup() {
+  createCanvas(400,400);
+caixa = createSprite(200,200,30,30);
+}
+
+
+function draw() 
+{
+  background(200);
+  if (keyIsDown(RIGHT_ARROW)){
+    caixa.position.x = caixa.position.x + 5;
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    caixa.position.x = caixa.position.x - 5;
+  }
+  if (keyIsDown(UP_ARROW)){
+    caixa.position.y = caixa.position.y - 5;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    caixa.position.y = caixa.position.y + 5;
+  }
+drawSprites()
+}
+
+
+
+
